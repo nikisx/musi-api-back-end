3ea1b7dd1404490cb4d0b3fcda39c6009258de61
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2022-06-05 17:26:06 +0300

    Add files via upload

---
+++ JsonModels/AlbumJsonModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MusicApi.JsonModels
+{
+    public class AlbumJsonModel
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public double Duration { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string CreatedById { get; set; }
+
+        public IEnumerable<TrackJsonModel> Tracks { get; set; }
+    }
+}
---
+++ JsonModels/PlayListJsonModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace MusicApi.JsonModels
+{
+    public class PlayListJsonModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsPublic { get; set; }
+        public int TrackCount { get; set; }
+
+        public double Duration { get; set; }
+        public string CreatedById { get; set; }
+
+
+        public IEnumerable<TrackJsonModel> Tracks { get; set; }
+    }
+}
