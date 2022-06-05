42883a392f672257a60ba2e1f24ccabcf98d91a9
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2022-06-05 17:24:39 +0300

    add controllers

---
+++ Controllers/AlbumController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicApi.Common;
+using MusicApi.JsonModels;
+using MusicApi.Services.Albums;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace MusicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        private IAlbumService albumService;
+
+        public AlbumController(IAlbumService albumService)
+        {
+            this.albumService = albumService;
+        }
+
+        [Route("get-all")]
+        public CustomResponse<List<AlbumJsonModel>> GetAll()
+        {
+            var result = this.albumService.GetAlbums();
+
+            return result;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("create-album")]
+        public CustomResponse<List<AlbumJsonModel>> CreateAlbum([FromBody] AlbumJsonModel model)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var result = this.albumService.CreateAlbum(model, userId);
+
+            return result;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("edit-album")]
+        public CustomResponse<List<AlbumJsonModel>> EditAlbum([FromBody] AlbumJsonModel model)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var result = this.albumService.EditAlbum(model, userId);
+
+            return result;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("delete")]
+        public CustomResponse<bool> DeleteAlbum([FromBody] AlbumJsonModel model)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var result = this.albumService.DeleteAlbum(model.Id, userId);
+
+            return result;
+        }
+    }
+}
---
+++ Controllers/PlaylistController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicApi.Common;
+using MusicApi.JsonModels;
+using MusicApi.Services.Playlists;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace MusicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistController : ControllerBase
+    {
+        private IPlayListService playListService;
+
+        public PlaylistController(IPlayListService playListService)
+        {
+            this.playListService = playListService;
+        }
+
+        [Route("get-all")]
+        public CustomResponse<List<PlayListJsonModel>> GetAll()
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var result = this.playListService.GetPlayLists(userId);
+
+            return result;
+        }
+
+        [Route("get-playlist")]
+        public CustomResponse<PlayListJsonModel> GetPlaylist([FromQuery] int id)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var result = this.playListService.GetPlaylist(id, userId);
+
+            return result;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("create-playlist")]
+        public CustomResponse<List<PlayListJsonModel>> CreatePlaylist([FromBody] PlayListJsonModel model)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var result = this.playListService.CreatePlayList(model, userId);
+
+            return result;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("edit-playlist")]
+        public CustomResponse<List<PlayListJsonModel>> EditPlaylist([FromBody] PlayListJsonModel model)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var result = this.playListService.EditPlayList(model, userId);
+
+            return result;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("delete")]
+        public CustomResponse<bool> Delete([FromBody] PlayListJsonModel model)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var result = this.playListService.DeletePlayList(model.Id, userId);
+
+            return result;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("add-track-to-playlist")]
+        public CustomResponse<bool> AddTrack([FromForm] int trackId, [FromForm] int playlistId)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var result = this.playListService.AddTrack(trackId, playlistId, userId);
+
+            return result;
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        [Route("change-track-position")]
+        public CustomResponse<PlayListJsonModel> ChangeTrackPosition([FromForm] int trackId, [FromForm] int playlistId, [FromForm] int position)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var result = this.playListService.ChangeTrackPosition(trackId, playlistId,position, userId);
+
+            return result;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("remove-track")]
+        public CustomResponse<bool> RemoveTackFromPlaylist([FromForm] int trackId, [FromForm] int playlistId)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var result = this.playListService.RemoveTrackFromPlaylist(trackId, playlistId, userId);
+
+            return result;
+        }
+    }
+}
