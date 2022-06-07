b43efaa430f725cb426a1fbb2412c4f59846e569
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2022-06-07 17:07:54 +0300

    Add files via upload

---
+++ Migrations/20220607140217_AddDurationForPlaylist.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicApi.Migrations
+{
+    public partial class AddDurationForPlaylist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Duration",
+                table: "Playlists",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Duration",
+                table: "Albums",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Playlists");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Duration",
+                table: "Albums",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
