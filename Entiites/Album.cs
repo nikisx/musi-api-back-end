0e1ac1b14bbb637e0d15b5137b3bafbe4ce696e0
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2022-06-07 16:53:32 +0300

    Update Album.cs

--- Entiites/Album.cs
+++ Entiites/Album.cs
@@ -6,7 +6,7 @@ namespace MusicApi.Entiites
     {
         public int Id { get; set; }
         public int Year { get; set; }
-        public int Duration { get; set; }
+        public double Duration { get; set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
 
