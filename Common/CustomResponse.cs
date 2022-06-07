43d8c16ef2fe500f6f8c09c84ebf104a4d563ae4
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2022-06-07 16:52:42 +0300

    add custom response

---
+++ Common/CustomResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicApi.Common
+{
+    public class CustomResponse<T>
+    {
+        public string Status { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
